b6bb0607353bb36a9a119a0503eac0615a9e85e9
A8 <161058439+A8T1N@users.noreply.github.com>
2025-04-21 22:41:10 +0900

    Add config to support legacy recipes

--- src/main/java/morecapacitor/recipe/alloyrecipe/RecipeSingularityAlloyIngot.java
+++ src/main/java/morecapacitor/recipe/alloyrecipe/RecipeSingularityAlloyIngot.java
@@ -6,6 +6,7 @@ import crazypants.enderio.base.recipe.RecipeInput;
 import crazypants.enderio.base.recipe.RecipeLevel;
 import crazypants.enderio.base.recipe.alloysmelter.AlloyRecipeManager;
 import morecapacitor.MoreCapacitor;
+import morecapacitor.config.MoreCapacitorConfig;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -13,15 +14,18 @@ import net.minecraft.item.ItemStack;
 
 public class RecipeSingularityAlloyIngot {
     public static void registerRecipes() {
-        ItemStack doubleCompressedInfinityDustBlcok = new ItemStack(Item.getByNameOrId("enderio:block_infinity"), 2, 2);
+        ItemStack doubleCompressedInfinityDustBlock = new ItemStack(Item.getByNameOrId("enderio:block_infinity"), 2, 2);
+        ItemStack reinforcedObsidianBlock = new ItemStack(Item.getByNameOrId("enderio:block_reinforced_obsidian"),2);
         ItemStack ingotStellar = new ItemStack(Item.getByNameOrId("enderio:item_alloy_endergy_ingot"), 2, 3);
-
         ItemStack enderEye = new ItemStack(Items.ENDER_EYE,2);
+
+        ItemStack thirdInput = MoreCapacitorConfig.legacyRecipeSupport ? reinforcedObsidianBlock : doubleCompressedInfinityDustBlock;
 
         NNList<IRecipeInput> inputs = new NNList<>(
                 new RecipeInput(enderEye, true),
                 new RecipeInput(ingotStellar, true),
-                new RecipeInput(doubleCompressedInfinityDustBlcok, true)
+//                new RecipeInput(doubleCompressedInfinityDustBlock, true)
+                new RecipeInput(thirdInput,true)
         );
 
         ItemStack outputItem = new ItemStack(MoreCapacitor.SINGULARITY_ALLOY_INGOT,1);
